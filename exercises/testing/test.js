9ee93cdb5cf2a31c23f972f62e6e430d19e0e56a
Tan Theng Lee <thengleetan@gmail.com>
2020-08-06 22:28:56 +0800

    Complete testing and debugging exercises

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,35 @@
 // write some tests
-describe('users', () => {
-  
-})
+const { findUser, deleteUser, fixId } = require("./users");
+
+describe("users", () => {
+  describe("fixId", () => {
+    test("converts param id string to id integer", () => {
+      expect(fixId("9")).toBe(9);
+    });
+  });
+
+  describe("findUser", () => {
+    test("finds user by id if user exists", async () => {
+      const user = await findUser(1);
+      expect(user.id).toBe(1);
+    });
+
+    test("returns no user found if user does not exists", async () => {
+      await expect(findUser(9999)).rejects.toThrow(`No user with id "9999"`);
+    });
+  });
+
+  describe("deleteUser", () => {
+    test("delete user by id if user exists", async () => {
+      const user = await findUser(2);
+      const deletedUser = await deleteUser(user.id);
+      expect(deletedUser.id).toBe(2);
+
+      try {
+        await findUser(2);
+      } catch (e) {
+        expect(e).toBeTruthy();
+      }
+    });
+  });
+});
--- exercises/testing/users.js
+++ exercises/testing/users.js
@@ -1,35 +1,40 @@
-const users = new Array(20).fill(0)
-.map((_, i) => {
+const users = new Array(20).fill(0).map((_, i) => {
   return {
     id: i,
     createdAt: Date.now() + i,
     email: `readycoder${i}@gmail.com`
-  }
-})
+  };
+});
 
 // simulate async db call with promise
-const findUser = (id) => new Promise((resolve, reject) => {
-  const user = users.find(user => user.id === id)
-  if (user) {
-    return resolve(user)
-  }
-  reject(new Error(`No user with id "${id}"`))
-})
+const findUser = id =>
+  new Promise((resolve, reject) => {
+    const fixedId = fixId(id);
+    const user = users.find(user => user.id === fixedId);
+    if (user) {
+      return resolve(user);
+    }
+    reject(new Error(`No user with id "${id}"`));
+  });
 
 // simulate async db call with promise
-const deleteUser = (id) => new Promise((resolve, reject) => {
-  const id = fixId(id)
-  const i = users.findIndex(user => user.id === id)
+const deleteUser = id =>
+  new Promise((resolve, reject) => {
+    const fixedId = fixId(id);
+    const i = users.findIndex(user => user.id === fixedId);
 
-  if (i < 0) {
-    return reject(new Error(`No user with id "${id}"`))
-  }
+    if (i < 0) {
+      return reject(new Error(`No user with id "${id}"`));
+    }
 
-  users.slice(i, 1)
-  resolve({id})
-})
+    const u = users.splice(i, 2);
+    resolve({ id });
+  });
+
+const fixId = id => Number(id);
 
 module.exports = {
   findUser,
-  deleteUser
-}
+  deleteUser,
+  fixId
+};
